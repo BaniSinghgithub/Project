cd7d6810bbae0d830b11d90f63abed3cdb2f8d69
BANI SINGH <141717536+BaniSinghgithub@users.noreply.github.com>
2025-03-27 15:18:16 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,14 @@ require("dotenv").config();  // to access environmental variables from .env file
 connectDB();
 
 const app = express();   // to make it executable
+app.use(express.json());
+
 const corsOptions = {
   origin: ["http://localhost:3000", "https://project-nhyt-ag4s0vn44-bani-singhs-projects.vercel.app"],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
-app.use(express.json());
 app.use("/api", userRoutes);
 
 // console.log(process.env.PORT);  
