816ade8032e4941e7a46e2639419d2af3785514c
BANI SINGH <141717536+BaniSinghgithub@users.noreply.github.com>
2025-04-19 01:51:57 +0530

    Update signUp.jsx

--- frontend/src/signUp.jsx
+++ frontend/src/signUp.jsx
@@ -118,7 +118,7 @@ export default function SignUp() {
 
       // If user not exist, register
       const registerResponse = await axios.post(
-        `${API_URL}/api/userRoutes/register`,
+        `${API_URL}/api/register`,
         {
           name: userData.name,
           email: userData.email,
@@ -177,7 +177,7 @@ export default function SignUp() {
 
     try {
       const registerResponse = await axios.post(
-        `${API_URL}/api/userRoutes/register`,
+        `${API_URL}/api/register`,
         {
           name: formData.username,
           email: formData.email,
